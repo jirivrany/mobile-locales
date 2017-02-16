1beac8e816d7278fa351ce2276de0cf885d4401e
Vladimir Belohradsky <vladimir.belohradsky@gmail.com>
2017-02-16 16:02:55 +0100

    iOS Base language configurable with config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 # supported languages - ISO code
 LANGS = ['fr', 'en', 'de', 'it', 'sk', 'cs', 'es', 'pt', 'ja']
+# base language for iOS
+BASE_LANG = 'en'
 # url with google sheet 
 DATA_URL = 'https://docs.google.com/spreadsheets/d/14L6Xrcwvs_DiLb0H3NApgwSrHf0BGgttuBlq1YsFmBQ/export?format=csv'
